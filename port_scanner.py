d8cd60cd236dd0575e2eb56b19798eee61dae06b
Gargi Bhardwaj <bhardwajgargi.1@gmail.com>
2025-04-03 03:42:20 -0400

    avs

--- port_scanner.py
+++ port_scanner.py
@@ -7,6 +7,7 @@ import json
 import csv
 import time
 from tqdm import tqdm
+#Gargi pagal h
 
 # Usage information
 usage = "Usage: python3 port_scan.py TARGET START_PORT END_PORT [--open | --filtered | --closed | --export <format>]"
